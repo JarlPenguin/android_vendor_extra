2791601a190d0f4ff51bec78346db706c464b4fd
Jarl-Penguin <jarlpenguin@outlook.com>
2021-07-02 17:33:01 +0300

    extra: Include AudioFX again and drop custom Updater URL
    * This will go to mod branch
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- product.mk
+++ product.mk
@@ -3,4 +3,3 @@ PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
 
 # Updater
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.allow_downgrading=true
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += lineage.updater.uri="https://github.com/moto-msm8937/releases/raw/master/{device}/lineage18.json"
