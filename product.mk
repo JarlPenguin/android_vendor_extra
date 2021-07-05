bcb29eb4b2d41f3dcc188933cf0224e8d3408b80
Jarl-Penguin <jarlpenguin@outlook.com>
2021-07-05 09:16:55 +0000

    extra: Add configuration for river
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- product.mk
+++ product.mk
@@ -1,5 +1,11 @@
 # Apex
 $(call inherit-product, vendor/extra/config/apex.mk)
 
+# Enable Google Play system updates for river
+ifneq ($(filter %river,$(TARGET_PRODUCT)),)
+MAINLINE_INCLUDE_WIFI_MODULE := true
+include vendor/partner_modules/build/mainline_modules.mk
+endif
+
 # Signing
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
