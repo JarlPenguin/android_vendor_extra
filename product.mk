0bf31d1a9e42b2a8d4f641085a5b784e2f30e4b5
Jarl-Penguin <jarlpenguin@outlook.com>
2022-01-03 08:08:04 +0100

    extra: Kang APEX setup from hOS
    * As of https://github.com/hentaiOS/platform_vendor_hentai/commit/11c69b19126f713996e426d303b93b6d85732f37
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- product.mk
+++ product.mk
@@ -1,6 +1,3 @@
-# Apex
-$(call inherit-product, vendor/extra/config/apex.mk)
-
 # CarrierSettings
 ifneq ($(filter %river,$(TARGET_PRODUCT)),)
 $(call inherit-product, vendor/extra/config/carrier_settings.mk)
@@ -8,26 +5,7 @@ endif
 
 # Enable Google Play system updates for river
 ifneq ($(filter %river,$(TARGET_PRODUCT)),)
-MODULE_BUILD_FROM_SOURCE := false
-MAINLINE_COMPRESS_APEX_ADBD := false
-MAINLINE_COMPRESS_APEX_ART := false
-MAINLINE_COMPRESS_APEX_CELLBROADCAST := false
-MAINLINE_COMPRESS_APEX_CONSCRYPT := false
-MAINLINE_COMPRESS_APEX_RESOLV := false
-MAINLINE_COMPRESS_APEX_EXTSERVICES := false
-MAINLINE_COMPRESS_APEX_IPSEC := false
-MAINLINE_COMPRESS_APEX_MEDIA := false
-MAINLINE_COMPRESS_APEX_MEDIAPROVIDER := false
-MAINLINE_COMPRESS_APEX_MEDIASWCODEC := false
-MAINLINE_COMPRESS_APEX_NEURALNETWORKS := false
-MAINLINE_COMPRESS_APEX_STATSD := false
-MAINLINE_COMPRESS_APEX_PERMISSION := false
-MAINLINE_COMPRESS_APEX_SCHEDULING := false
-MAINLINE_COMPRESS_APEX_SDKEXT := false
-MAINLINE_COMPRESS_APEX_TETHERING := false
-MAINLINE_INCLUDE_WIFI_MODULE := true
-MAINLINE_COMPRESS_APEX_WIFI := false
-include vendor/partner_modules/build/mainline_modules.mk
+include vendor/extra/config/apex.mk
 endif
 
 # Signing
