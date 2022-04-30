9ae7141bfcca8fae2c259ecad08cb7278415045b
Jarl-Penguin <jarlpenguin@outlook.com>
2022-04-30 11:45:07 +0200

    extra: nio
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- product.mk
+++ product.mk
@@ -3,9 +3,15 @@ ifneq ($(filter %river,$(TARGET_PRODUCT)),)
 $(call inherit-product, vendor/extra/config/carrier_settings.mk)
 endif
 
-# Enable Google Play system updates for river
-ifneq ($(filter %river,$(TARGET_PRODUCT)),)
+# Enable Google Play system updates for nio
+ifneq ($(filter %nio %river,$(TARGET_PRODUCT)),)
 include vendor/extra/config/apex.mk
+endif
+
+# NGA
+ifneq ($(filter %nio,$(TARGET_PRODUCT)),)
+PRODUCT_PACKAGES += \
+    NgaResources
 endif
 
 # Signing
